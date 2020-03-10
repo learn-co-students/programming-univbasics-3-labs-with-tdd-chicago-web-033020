8e1fafb18705d8eab7181a4c1021165ab300cd8e
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-10 05:21:30 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,21 @@
-# Add your variables here
+first_number=3.0
+second_number=1.1
+sum=first_number+second_number
+difference=first_number-second_number
+product=first_number*second_number
+quotient=first_number/second_number
+def adding(first_number,second_number)
+  puts "#{first_number}+#{second_number}=#{sum}"
+  end
+
+def subtracting(first_number,second_number)
+  puts "#{first_number}-#{second_number}=#{difference}"
+  end
+
+def multiplying(first_number,second_number)
+  puts "#{first_number}*#{second_number}=#{product}"
+  end
+
+def dividing(first_number,second_number)
+  puts "#{first_number}/#{second_number}=#{product}"
+  end
